e7516feb11339b3f113399fc68f71fa142f751ca
エビフライ <163741315+FriedShrimp5200a@users.noreply.github.com>
2024-11-02 18:46:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ async function getIpAddress() {
     const response = await fetch('https://api.ipify.org?format=json');
     const data = await response.json();
     const getip = document.getElementById('getip');
-        getip.innerHTML = data.ip;
+        getip.innerHTML = 'Your IP address is 'data.ip;
   } catch (error) {
     console.error('Error fetching IP address:', error);
   }
